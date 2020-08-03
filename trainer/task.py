23671694917bb0fd54b1d4ff0cc0f788b8e59716
David Aponte <david.aponte@benevolent.ai>
2020-08-02 20:05:51 -0400

    chore: adding random forest to example and added formatting

--- trainer/task.py
+++ trainer/task.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import sys
 import pandas as pd
-from sklearn import svm
+from sklearn import svm, ensemble
 from sklearn.externals import joblib
 
 # Fill in your Cloud Storage bucket name
@@ -13,17 +13,23 @@ BUCKET_NAME = '<YOUR_BUCKET_NAME>'
 
 
 # [START download-data]
+# TODO: separate into features module
 iris_data_filename = 'iris_data.csv'
 iris_target_filename = 'iris_target.csv'
 data_dir = 'gs://cloud-samples-data/ml-engine/iris'
 
 # gsutil outputs everything to stderr so we need to divert it to stdout.
-subprocess.check_call(['gsutil', 'cp', os.path.join(data_dir,
-                                                                                                        iris_data_filename),
-                                              iris_data_filename], stderr=sys.stdout)
-subprocess.check_call(['gsutil', 'cp', os.path.join(data_dir,
-                                                                                                        iris_target_filename),
-                                              iris_target_filename], stderr=sys.stdout)
+training_data = os.path.join(data_dir, iris_data_filename)
+training_labels = os.path.joing(data_dir, iris_target_filename)
+
+subprocess.check_call(
+    ['gsutil', 'cp', training_data, iris_data_filename],
+    stderr=sys.stdout,
+)
+subprocess.check_call(
+    ['gsutil', 'cp', training_labels, iris_target_filename],
+    stderr=sys.stdout,
+)
 # [END download-data]
 
 
@@ -38,20 +44,32 @@ iris_target = iris_target.reshape((iris_target.size,))
 
 
 # [START train-and-save-model]
-# Train the model
-classifier = svm.SVC(gamma='auto', verbose=True)
-classifier.fit(iris_data, iris_target)
+# Train the models
+svm_classifier = svm.SVC(gamma='auto', verbose=True)
+svm_classifier.fit(iris_data, iris_target)
+# randomf forest
+rf_classifier = ensemble.RandomForestClassifier()
+rf_classifier.fit(iris_data, iris_target)
 
 # Export the classifier to a file
-model_filename = 'model.joblib'
-joblib.dump(classifier, model_filename)
-# [END train-and-save-model]
-
+svm_model_filename = 'svm-model.joblib'
+joblib.dump(svm_classifier, svm_model_filename)
+rf_model_filename = 'rf-model.joblib'
+joblib.dump(rf_classifier, rf_model_filename)
 
 # [START upload-model]
 # Upload the saved model file to Cloud Storage
-gcs_model_path = os.path.join('gs://', BUCKET_NAME,
-                                  datetime.datetime.now().strftime('iris_%Y%m%d_%H%M%S'), model_filename)
-subprocess.check_call(['gsutil', 'cp', model_filename, gcs_model_path],
-                          stderr=sys.stdout)
+# TODO: using date as RUN ID
+current_date = datetime.datetime.now().strftime('iris_%Y%m%d_%H%M%S')
+gcs_svm_model_path = os.path.join('gs://', BUCKET_NAME, current_date, svm_model_filename)
+subprocess.check_call(
+    ['gsutil', 'cp', svm_model_filename, gcs_svm_model_path],
+     stderr=sys.stdout,
+)
+gcs_rf_model_path = os.path.join('gs://', BUCKET_NAME, current_date, rf_model_filename)
+subprocess.check_call(
+    ['gsutil', 'cp', rf_model_filename, gcs_rf_model_path],
+    stderr=sys.stdout,
+)
+
 # [END upload-model]
